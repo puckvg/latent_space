78bdbc4f9dd23f213ff42eac88925f7df0d73f3f
Puck <puck.vangerwen@gmail.com>
2020-06-19 13:27:48 +0200

    cleanup

--- scripts/check_files.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for f in *.log; do 
-    if [[ `tail -1 $f | grep -L "Normal termination"` ]]; then 
-        echo $f; 
-    fi; 
-done > check.txt
-
--- scripts/parse_rmsd.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash 
-cutoff=2.5
-for file in *_rmsd.txt; do 
-    rmsd=$(cat $file)
-    if (( $(echo "$rmsd > $cutoff" | bc -l) )); then
-        echo $rmsd
-    fi
-done
